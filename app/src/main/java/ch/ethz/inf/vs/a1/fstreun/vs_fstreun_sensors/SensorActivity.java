da99ca9f9c5f2a653b81eb1feec7a3719f6d6c82
streuseli <fstreun@student.ethz.ch>
2017-10-02 17:28:12 +0200

    added SensorActivity and removed example test classes

---
+++ app/src/main/java/ch/ethz/inf/vs/a1/fstreun/vs_fstreun_sensors/SensorActivity.java
@@ -0,0 +1,13 @@
+package ch.ethz.inf.vs.a1.fstreun.vs_fstreun_sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+    }
+}
